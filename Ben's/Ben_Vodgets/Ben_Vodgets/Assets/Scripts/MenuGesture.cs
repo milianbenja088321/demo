498c843d4830138fdee6cf9cfebde277afdd63c3
Benjamin milian <vinyelortiz@gmail.com>
2017-12-03 14:36:05 -0500

    added grabbing and raycasting

--- Ben's/Ben_Vodgets/Ben_Vodgets/Assets/Scripts/MenuGesture.cs
+++ Ben's/Ben_Vodgets/Ben_Vodgets/Assets/Scripts/MenuGesture.cs
@@ -3,41 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 
-[RequireComponent(typeof(SteamVR_TrackedController))]
+//[RequireComponent(typeof(SteamVR_TrackedController))]
 
 public class MenuGesture : MonoBehaviour
 {
 
-    SteamVR_TrackedController controller = null;
     public GameObject worldMenu = null;
-    LineRenderer line;
 
     bool isOpen = false;
 
     public float controllerRot;
-    public float angle = 30;
 
     // Use this for initialization
     void Start()
     {
-        controller = GetComponent<SteamVR_TrackedController>();
-
-        controller.TriggerClicked += OnClicked;
-        controller.TriggerUnclicked += OnUnClicked;
-
-
-        line = this.GetComponent<LineRenderer>();
-    }
-
-    void OnClicked(object sender, ClickedEventArgs e)
-    {
-        Debug.Log(controllerRot);
-        line.enabled = true;
-    }
-
-    void OnUnClicked(object sender, ClickedEventArgs e)
-    {
-        line.enabled = false;
     }
 
     void OpenMenu(bool _isActive = false, GameObject _toInstantiate = null)
@@ -48,14 +27,10 @@ public class MenuGesture : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        line.SetPosition(0, this.transform.position);
-        Quaternion q = Quaternion.AngleAxis(angle, transform.right);
-        Vector3 v = q * (this.transform.forward * 10);
-        Vector3 targetPos = this.transform.position + v;
-        line.SetPosition(1, targetPos);
+        
 
         controllerRot = this.transform.eulerAngles.z;
-        print(this.transform.localPosition);
+        //print(this.transform.localPosition);
 
         if(controllerRot >= 80 && controllerRot < 110)
         {
